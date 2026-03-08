575823b459dbe61bb2020356b4b00ab0c12e1123
Paulo Roberto <castroaureo8@gmail.com>
2026-03-08 17:11:33 -0300

    imagens-pwa

--- Projeto-Restaurante/dev-dist/sw.js
+++ Projeto-Restaurante/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-be266a9d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hg23d6bs1h"
+    "revision": "0.h2sq5q9clvg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- Projeto-Restaurante/vite.config.js
+++ Projeto-Restaurante/vite.config.js
@@ -36,17 +36,17 @@ export default defineConfig({
         theme_color: "#ef4444",
         icons: [
           {
-            src: "/icons/icon-192.png",
+            src: "/icons/logo-pedidos.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icons/icon-512.png",
+            src: "/icons/logo-pedidos-512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "/icons/icon-512-maskable.png",
+            src: "/icons/logo-pedidos-512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
